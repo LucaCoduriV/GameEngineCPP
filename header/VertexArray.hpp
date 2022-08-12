bc07034c8c5c3848a0278fd9d32415a6fd81f6b5
Luca <coduri.vitor@gmail.com>
2022-08-13 01:34:26 +0200

    VertexArray abstraction

---
+++ header/VertexArray.hpp
@@ -0,0 +1,22 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : VertexArray.hpp
+Auteur(s)       : lucac
+Date creation   : 12.08.2022
+Description     : <à compléter>
+Remarque(s)     : <à compléter>
+Compilateur     : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+
+*/
+
+#ifndef GAMEENGINE_VERTEXARRAY_HPP
+#define GAMEENGINE_VERTEXARRAY_HPP
+
+
+class VertexArray {
+
+};
+
+
+#endif //GAMEENGINE_VERTEXARRAY_HPP
---
+++ header/VertexBufferLayout.hpp
@@ -0,0 +1,22 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : VertexBufferLayout.hpp
+Auteur(s)       : lucac
+Date creation   : 12.08.2022
+Description     : <à compléter>
+Remarque(s)     : <à compléter>
+Compilateur     : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+
+*/
+
+#ifndef GAMEENGINE_VERTEXBUFFERLAYOUT_HPP
+#define GAMEENGINE_VERTEXBUFFERLAYOUT_HPP
+
+
+class VertexBufferLayout {
+
+};
+
+
+#endif //GAMEENGINE_VERTEXBUFFERLAYOUT_HPP
