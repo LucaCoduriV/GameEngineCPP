8b733ba8397bb85093092122f54be89162525029
Luca <coduri.vitor@gmail.com>
2022-08-15 19:20:41 +0200

    Added Ecs lib + cmake file for it

---
+++ Ecs/Ecs.hpp
@@ -0,0 +1,20 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : Ecs.hpp
+Auteur(s)       : lucac
+Date creation   : 15.08.2022
+Description     : <à compléter>
+Remarque(s)     : <à compléter>
+Compilateur     : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+
+*/
+
+#ifndef SANDBOX_ECS_HPP
+#define SANDBOX_ECS_HPP
+
+int componentCounter = 0;
+
+
+
+#endif //SANDBOX_ECS_HPP
---
+++ Sandbox/EcsTest/main.cpp
@@ -0,0 +1,20 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : main.cpp
+Auteur(s)       : lucac
+Date creation   : 15.08.2022
+Description     : <à compléter>
+Remarque(s)     : <à compléter>
+Compilateur     : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+
+*/
+#include <cstdlib>
+#include <iostream>
+#include <Ecs.hpp>
+
+int main(){
+   std::cout << "hello world !" << std::endl;
+   return EXIT_SUCCESS;
+}
+
