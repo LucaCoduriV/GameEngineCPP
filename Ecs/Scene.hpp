0bc6bbda46889d98d849aff14e2055ae716a4cf9
Luca <coduri.vitor@gmail.com>
2022-08-16 01:13:42 +0200

    Ecs refactor

---
+++ Ecs/Scene.hpp
@@ -0,0 +1,121 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : Scene.hpp
+Auteur(s)       : lucac
+Date creation   : 15.08.2022
+Description     : <à compléter>
+Remarque(s)     : <à compléter>
+Compilateur     : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+
+*/
+
+#ifndef SANDBOX_SCENE_HPP
+#define SANDBOX_SCENE_HPP
+
+
+#include "functions.hpp"
+#include "ComponentPool.hpp"
+
+namespace ECS {
+
+   class Scene {
+      template<typename...> friend class SceneView;
+   public:
+      EntityID newEntity() {
+
+         // si pas vide on peur réutiliser un ancien emplacement
+         if (!freeEntities.empty()) {
+            EntityIndex newIndex = freeEntities.back();
+            freeEntities.pop_back();
+
+            EntityID newId = createEntityId(newIndex,
+                                            getEntityVersion(entities[newIndex].id));
+            entities[newIndex].id = newId;
+            return newId;
+         }
+
+         entities.push_back({createEntityId(EntityIndex(entities.size()), 0),
+                             ComponentMask()});
+
+         return entities.back().id;
+      };
+
+      void destroyEntity(EntityID id) {
+         EntityID newID = createEntityId(EntityIndex(-1), getEntityVersion(id) + 1);
+         entities[getEntityIndex(id)].id = newID;
+         entities[getEntityIndex(id)].mask.reset();
+         freeEntities.push_back(getEntityIndex(id));
+      }
+
+      template<typename T>
+      T *assign(EntityID id) {
+         auto componentId = getComponentId<T>();
+
+         auto entityIndex = getEntityIndex(id);
+         if(entities[entityIndex].id != id){
+            return nullptr;
+         }
+
+         // on utilise pas le pushback car son implémentation double la mémoire
+         // s'il y en a pas assez.
+
+         if (componentPools.size() <= componentId) { // Pas assez de mémoire
+            componentPools.resize(componentId + 1, nullptr);
+         }
+         if (componentPools[componentId] == nullptr) { // Il faut donc en créer un
+            componentPools[componentId] = new ComponentPool(sizeof(T));
+         }
+
+         // on crée un nouvel élément à cet emplacement mémoire
+         auto *pComponent = new(componentPools[componentId]->get(entityIndex)) T();
+
+         entities[entityIndex].mask.set(componentId);
+
+         return pComponent;
+      }
+
+      template<typename T>
+      T *get(EntityID id) {
+         auto componentId = getComponentId<T>();
+
+         auto entityIndex = getEntityIndex(id);
+         if (entities[entityIndex].id != id) {
+            return nullptr;
+         }
+
+         if (!entities[entityIndex].mask.test(componentId)) {
+            return nullptr;
+         }
+
+         T *pComponent = static_cast<T *>(componentPools[componentId]->get(
+            entityIndex));
+         return pComponent;
+      }
+
+      template<typename T>
+      void remove(EntityID id) {
+
+         auto entityIndex = getEntityIndex(id);
+
+         if (entities[entityIndex].id != id) {
+            return;
+         }
+
+         auto componentId = getComponentId<T>();
+
+         entities[entityIndex].mask.reset(componentId);
+
+      }
+   private:
+      struct EntityDesc {
+         EntityID id;
+         ComponentMask mask;
+      };
+
+      std::vector<EntityDesc> entities;
+      std::vector<ComponentPool *> componentPools;
+      std::vector<EntityIndex> freeEntities;
+   };
+}
+#endif //SANDBOX_SCENE_HPP
--- Sandbox/EcsTest/main.cpp
+++ Sandbox/EcsTest/main.cpp
@@ -13,7 +13,6 @@ Compilateur     : Mingw-w64 g++ 8.1.0
 #include <iostream>
 #include <Ecs.hpp>
 
-//int ECS::s_componentCounter = 0;
 
 struct TransformComponent{
    float x;
@@ -34,27 +33,37 @@ int main(){
 
    auto voiture1 = scene.newEntity();
    auto voiture2 = scene.newEntity();
+   auto voiture3 = scene.newEntity();
 
    scene.assign<TransformComponent>(voiture1);
-   scene.assign<TransformComponent>(voiture2);
+
+   scene.assign<Transform2Component>(voiture2);
+
+   scene.assign<TransformComponent>(voiture3);
+   scene.assign<Transform2Component>(voiture3);
 
    auto t1  = scene.get<TransformComponent>(voiture1);
    t1->x = 10;
    t1->y = 30;
    t1->z = 50;
-   auto t2  = scene.get<TransformComponent>(voiture2);
+   auto t2  = scene.get<Transform2Component>(voiture2);
    t2->x = 5;
    t2->y = 3;
    t2->z = 2;
 
-   std::cout << t1->x << " " << t1->y << " " << t1->z << std::endl;
-   std::cout << t2->x << " " << t2->y << " " << t2->z << std::endl;
+   auto t3  = scene.get<Transform2Component>(voiture3);
+   t3->x = 5;
+   t3->y = 3;
+   t3->z = 2;
 
-   auto t3 = scene.get<TransformComponent>(voiture1);
-   auto t4 = scene.get<TransformComponent>(voiture2);
+   auto t4  = scene.get<TransformComponent>(voiture3);
+   t4->x = 5;
+   t4->y = 3;
+   t4->z = 2;
 
-   std::cout << t3->x << " " << t3->y << " " << t3->z << std::endl;
-   std::cout << t4->x << " " << t4->y << " " << t4->z << std::endl;
+   for(ECS::EntityID id : ECS::SceneView<TransformComponent>(scene)){
+      std::cout << ECS::getEntityIndex(id) + 1 << std::endl;
+   }
 
    return EXIT_SUCCESS;
 }
