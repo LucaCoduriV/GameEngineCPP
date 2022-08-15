58bd049b61781227b66d728d1aa021f5a6d22d1c
Luca <coduri.vitor@gmail.com>
2022-08-15 20:28:38 +0200

    Ecs base

--- Ecs/Ecs.hpp
+++ Ecs/Ecs.hpp
@@ -13,7 +13,104 @@ Compilateur     : Mingw-w64 g++ 8.1.0
 #ifndef SANDBOX_ECS_HPP
 #define SANDBOX_ECS_HPP
 
-int componentCounter = 0;
+
+#include <bitset>
+#include <vector>
+#include <limits>
+
+namespace ECS{
+   static int componentCounter = 0;
+   const int MAX_COMPONENTS = 32;
+
+   using ComponentMask = std::bitset<MAX_COMPONENTS>;
+   using EntityID = unsigned long long;
+
+   const unsigned long long MAX_ENTITIES = 5000;
+
+
+
+   template <typename T>
+   int getId(){
+      static int componentId = componentCounter++;
+      return componentId;
+   }
+
+   struct ComponentPool {
+      ComponentPool(size_t elementSize){
+         this->elementSize = elementSize;
+         pData = new char[elementSize * MAX_ENTITIES];
+      }
+
+      ~ComponentPool(){
+         delete[] pData;
+      }
+
+      inline void* get(size_t index){
+         return &pData[index * elementSize];
+      }
+
+      char* pData = nullptr;
+      size_t elementSize = 0;
+   };
+
+   struct Scene{
+      struct EntityDesc {
+         EntityID id;
+         ComponentMask mask;
+      };
+
+      std::vector<EntityDesc> entities;
+      std::vector<ComponentPool*> componentPools;
+
+      EntityID newEntity(){
+         entities.push_back({entities.size(), ComponentMask()});
+         return entities.back().id;
+      };
+
+      template<typename T>
+      T* assign(EntityID id){
+         auto componentId = getId<T>();
+
+         if(componentPools.size() <= componentId) { // Pas assez de mémoire
+            componentPools.resize(componentId + 1, nullptr);
+         }
+         if(componentPools[componentId] == nullptr){ // Il faut donc en créer un
+            componentPools[componentId] = new ComponentPool(sizeof(T));
+         }
+
+         //Solution temporaire mais utilise plus de mémoire
+         //componentPools.push_back(new ComponentPool(sizeof(T)));
+
+         // on crée un nouvel élément à cet emplacement mémoire
+         auto* pComponent = new (componentPools[componentId]->get(id)) T();
+
+         entities[id].mask.set(componentId);
+
+         return pComponent;
+      }
+
+      template<typename T>
+      T* get(EntityID id){
+         auto componentId = getId<T>();
+
+         if(!entities[id].mask.test(componentId)){
+            return nullptr;
+         }
+
+         T* pComponent = static_cast<T*>(componentPools[componentId]->get(id));
+         return pComponent;
+      }
+
+
+   };
+
+
+
+
+
+
+}
+
 
 
 
--- Sandbox/EcsTest/main.cpp
+++ Sandbox/EcsTest/main.cpp
@@ -13,8 +13,49 @@ Compilateur     : Mingw-w64 g++ 8.1.0
 #include <iostream>
 #include <Ecs.hpp>
 
+//int ECS::s_componentCounter = 0;
+
+struct TransformComponent{
+   float x;
+   float y;
+   float z;
+};
+
+struct Transform2Component{
+   float x;
+   float y;
+   float z;
+};
+
 int main(){
-   std::cout << "hello world !" << std::endl;
+
+
+   ECS::Scene scene;
+
+   auto voiture1 = scene.newEntity();
+   auto voiture2 = scene.newEntity();
+
+   scene.assign<TransformComponent>(voiture1);
+   scene.assign<TransformComponent>(voiture2);
+
+   auto t1  = scene.get<TransformComponent>(voiture1);
+   t1->x = 10;
+   t1->y = 30;
+   t1->z = 50;
+   auto t2  = scene.get<TransformComponent>(voiture2);
+   t2->x = 5;
+   t2->y = 3;
+   t2->z = 2;
+
+   std::cout << t1->x << " " << t1->y << " " << t1->z << std::endl;
+   std::cout << t2->x << " " << t2->y << " " << t2->z << std::endl;
+
+   auto t3 = scene.get<TransformComponent>(voiture1);
+   auto t4 = scene.get<TransformComponent>(voiture2);
+
+   std::cout << t3->x << " " << t3->y << " " << t3->z << std::endl;
+   std::cout << t4->x << " " << t4->y << " " << t4->z << std::endl;
+
    return EXIT_SUCCESS;
 }
 
