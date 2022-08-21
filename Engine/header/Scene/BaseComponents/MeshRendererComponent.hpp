5490d07b990fbf12bdce30be0656a043bf53a85b
Luca <coduri.vitor@gmail.com>
2022-08-21 04:35:46 +0200

    MeshRenderer

---
+++ Engine/header/Scene/BaseComponents/MeshRendererComponent.hpp
@@ -0,0 +1,39 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : Renderer3D.hpp
+Auteur(s)       : lucac
+Date creation   : 21.08.2022
+Description     : <à compléter>
+Remarque(s)     : <à compléter>
+Compilateur     : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+
+*/
+
+#ifndef ENGINE_MESHRENDERERCOMPONENT_HPP
+#define ENGINE_MESHRENDERERCOMPONENT_HPP
+
+#include <Shader.hpp>
+#include <VertexBuffer.hpp>
+#include <IndexBuffer.hpp>
+#include <definitions.hpp>
+
+namespace GE{
+   class MeshRendererComponent {
+   public:
+      MeshRendererComponent() = default;
+
+      void MeshRendererComponent::init(const std::vector<Vertex>& vertices,
+                                       const std::vector<unsigned int>& uvs);
+      void draw(const Shader &shader);
+
+   private:
+      GE::Ref<VertexArray> vao;
+      Ref<VertexBuffer> vbo;
+      Ref<IndexBuffer> ebo;
+   };
+
+
+}
+
+#endif //ENGINE_MESHRENDERER_HPP
---
+++ Engine/src/Scene/BaseComponents/MeshRendererComponent.cpp
@@ -0,0 +1,50 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : Renderer3D.cpp
+Auteur(s)       : lucac
+Date creation   : 21.08.2022
+Description     : <à compléter>
+Remarque(s)     : <à compléter>
+Compilateur     : Mingw-w64 g++ 8.1.0
+-----------------------------------------------------------------------------------
+
+*/
+
+#include <ModelLoader/Mesh.hpp>
+#include "Scene/BaseComponents/MeshRendererComponent.hpp"
+
+namespace GE{
+   void MeshRendererComponent::init(const std::vector<Vertex>& vertices,
+                                   const std::vector<unsigned int>& uvs){
+      vao = std::make_shared<VertexArray>();
+      vbo = std::make_shared<VertexBuffer>(vertices.data(), vertices.size() * sizeof(Vertex));
+      ebo = std::make_shared<IndexBuffer>(uvs.data(), uvs.size());
+
+      VertexBufferLayout layout;
+      layout.push<float>(3);
+      layout.push<float>(3);
+      layout.push<float>(2);
+      layout.push<float>(3);
+      layout.push<float>(3);
+      layout.push<int>(4, BufferType::INT);
+      layout.push<float>(4);
+      vao->addBuffer(*vbo, layout);
+   }
+
+   void MeshRendererComponent::draw(const Shader &shader){
+      // bind appropriate textures
+      unsigned int diffuseNr  = 1;
+      unsigned int specularNr = 1;
+      unsigned int normalNr   = 1;
+      unsigned int heightNr   = 1;
+
+      // draw mesh
+      vao->bind();
+      ebo->bind();
+      GLCall(glDrawElements(GL_TRIANGLES, ebo->getCount(), GL_UNSIGNED_INT,
+                            0));
+
+      VertexArray::unbind();
+
+   }
+}
